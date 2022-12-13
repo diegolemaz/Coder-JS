9cb37b2f344298595ec08376f0b7881ea30e6ea8
Diego Lema <dlz85@outlook.com>
2022-12-13 00:58:52 -0300

    Funciones E-commerce

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,79 @@
-// DATOS DE DE USUARIO E INICIALIZACION DE VARIABLES
-
-let usuarioBase = "diegol";
-let passwordBase = "pass";
-let usuarioConEmail;
-let ingresoConExito = false;
-
-// LOGIN
-let nombre = prompt("Ingrese su nombre:");
-let usuario = prompt("Ingrese su usuario:");
-let password = prompt("Ingrese su contraseña:");
-
-// CONTROL DE LOGIN
-function controlIngreso(par1, par2, par3, par4) {
-  if (par1 === par2) {
-    if (par3 === par4) {
-      alert("bienvenido " + nombre);
-      ingresoConExito = true;
-    } else {
-      alert("Su contraseña es errónea");
-    }
-  } else {
-    if (par3 === par4) {
-      alert("Su usuario es erróneo");
-    } else {
-      alert("Su usuario y contraseña son erróneos");
-    }
+class Producto {
+  constructor(id, descripcion, marca, presentacion, categoria, precio, oferta, fechaVto) {
+      this.id = id;
+      this.descripcion = descripcion.toUpperCase();
+      this.marca = marca.toUpperCase();
+      this.presentacion = presentacion.toUpperCase();
+      this.categoria = categoria.toUpperCase();
+      this.precio = precio;
+      this.oferta = oferta;
+      this.fechaVto = new Date(fechaVto);
   }
 }
 
-// FUNCION CONTROL DE USUARIO CON MAIL
-function controlMail(par1, par2) {
-  if (par2) {
-    let contador = 0;
-    for (let i = 0; i < par1.length; i++) {
-      if (par1[i].match("@")) {
-        contador = contador + 1;
+const productos = [];
+productos.push(new Producto("001", "Cerveza", "Budwisser", "355 ml", "Cervezas", "45", true, "2025,1,10"));
+productos.push(new Producto("002", "Cerveza", "Corona", "355 ml", "Cervezas", "55", false));
+productos.push(new Producto("003", "Cerveza", "Coronita", "210 ml", "Cervezas", "30", false));
+productos.push(new Producto("004", "Cerveza Ipa", "Goose Island", "473 ml", "Cervezas", "49", false));
+productos.push(new Producto("005", "Cerveza", "Patricia", "473 ml", "Cervezas", "49", true));
+productos.push(new Producto("006", "Cerveza (envase retornable)", "Patricia", "340 ml", "Cervezas", "25", true));
+productos.push(new Producto("007", "Cerveza", "Zillertal", "473 ml", "Cervezas", "53", false));
+productos.push(new Producto("008", "Aperitivo", "Campari", "750 ml", "Aperitivos", "600", true));
+productos.push(new Producto("009", "Gin tonic", "Dr Lemon", "lata 310 ml", "Tragos", "150", true));
+productos.push(new Producto("010", "Fernet", "Branca", "750 ml", "Aperitivos", "300", true));
+productos.push(new Producto("011", "Gin", "Alquimista", "500 ml", "Blancas", "600", false));
+productos.push(new Producto("012", "Gin", "Gordons", "500 ml", "Blancas", "600", false));
+productos.push(new Producto("013", "Gin", "Tanqueray", "750 ml", "Blancas", "600", false));
+productos.push(new Producto("014", "Licor", "Jagermeifter", "700 ml", "Licores", "600", false));
+productos.push(new Producto("015", "Ron Añejo Especial", "Havana", "750 ml", "Blancas", "800", false));
+productos.push(new Producto("016", "Vodka", "Smirnoff", "750 ml", "Blancas", "600", true));
+
+
+
+// FUNCION CAMBIAR DE PRECIO DTO
+
+const cambiarPrecioDto = (dto) => {
+  productos.forEach(producto => {
+      if (producto.oferta == true) {
+          producto.precio = producto.precio * ((100 - dto) / 100);
+          producto.descripcion = producto.descripcion + " (Precio Oferta)";
       }
-    }
-    // CAMBIO DE USUARIO SIN @ AL MAIL INGRESADO DESDE PROMPT
-    if (contador == 0) {
-      let usuarioConEmail = prompt("Ingrese nuevo email:");
-      usuarioBase = usuarioConEmail;
-    }
+  })
+}
+cambiarPrecioDto(10);
+console.log(productos);
+
+// FUNCION ORDENAR ARAY X DESCRIPCION
+
+const ordenarListadoDesc = (prod1, prod2) => {
+
+  if (prod1.descripcion > prod2.descripcion) {
+      return 1
+  }
+  if (prod1.descripcion < prod2.descripcion) {
+      return -1
+  }
+  if (prod1.descripcion = prod2.descripcion) {
+      return 0
   }
 }
+let listadoOrdenado = productos.slice();
+listadoOrdenado.sort(ordenarListadoDesc);
 
-// FUNCION CONTROL DE PASSWORD 
-function controlPassword(par1, par2) {
-  // CONTROL SI ES MENOR A 8 CARCTERES
-  if (par2) {
-    if (par1.length <= 8) {
-      alert("Su contraseña es insegura");
-    }
-    // CONTROL SI TIENE ALGUN NUMERO
-    let contador = 0;
-    for (let i = 0; i < par1.length; i++)
-      if ("0123456789".match(par1[i])) {
-        contador = contador + 1;
-      }
-    // CAMBIO DE PASSWORD INGRESADA DESDE PROMPT
-    if (contador == 0) {
-      let varResp = prompt(
-        "Su contraseña debe tener caracteres alfanuméricos. Desea cambiarla?");
-      if (varResp == "SI" || varResp == "si" || varResp == "Si") {
-        let nuevoPassword = prompt("ingrese su nueva contraseña");
-        passwordBase = nuevoPassword;
-      }
-    }
-  }
+console.log(listadoOrdenado);
+
+// FUNCION FILTRAR ARRAY SOLO OFERTAS
+
+const mostrarProductosOferta = (producto) => {
+  return producto.oferta == true
 }
+const productosFiltrados = productos.filter(mostrarProductosOferta);
+console.log(productosFiltrados);
 
-controlIngreso(usuarioBase, usuario, passwordBase, password);
-controlMail(usuarioBase, ingresoConExito);
-controlPassword(passwordBase, ingresoConExito);
+//FUNCION BUSCAR Y MOSTRAR ALERT PRIMER ELEMENTO
+
+let productoBuscado = prompt("Ingrese el producto que desea buscar:");
+productoBuscado = productoBuscado.toLocaleUpperCase();
+const busqueda = productos.find((producto) => producto.descripcion.includes(productoBuscado));
+alert(`${busqueda.descripcion} ${busqueda.marca} ${busqueda.presentacion} $ ${busqueda.precio}`);
